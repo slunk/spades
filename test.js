f7cef72070c5e8765d7f5c046d03cdb43097ea09
Peter Enns <slunk@umd.edu>
2013-09-24 14:51:03 -0400

    Separated some unit tests into more well-defined units

--- test.js
+++ test.js
@@ -12,55 +12,66 @@ describe("Model's", function () {
         done();
     });
 
-    describe("initialized", function () {
-        it("teams should begin with a score of 0", function (done) {
+    describe("reset method", function () {
+        it("should set both teams' scores to 0", function (done) {
             assert.equal(game.team0.score, 0);
             assert.equal(game.team1.score, 0);
             done();
         });
-        it("teams should begin with 13 cards", function (done) {
+        it("should deal each team 13 cards", function (done) {
             assert.equal(game.team0.player0.cards.length, 13)
             assert.equal(game.team0.player1.cards.length, 13)
             assert.equal(game.team1.player0.cards.length, 13)
             assert.equal(game.team1.player1.cards.length, 13)
             done();
         });
-        it("rounds should be correctly initialized", function (done) {
+        it("should initialize the current round's state", function (done) {
             assert.equal(game.roundInfo.turn, 1);
             assert.deepEqual(game.roundInfo.currBook, []);
             assert.deepEqual(game.roundInfo.team0.books, []);
             assert.deepEqual(game.roundInfo.team0.bid, {blind: true});
             assert.deepEqual(game.roundInfo.team1.books, []);
             assert.deepEqual(game.roundInfo.team1.bid, {blind: true});
+            assert.equal(game.roundInfo.team0.bid.blind, true);
             done();
         });
-        it("current player and team should be correctly initialized", function (done) {
+        it("should set up the current player and current team", function (done) {
             assert.deepEqual(game.currPlayer, {team: "team1", player: "player0"});
             assert.equal(game.currTeam, "team0");
             done();
         });
     });
 
-    describe("bidding", function (done) {
-        it("should start blind", function (done) {
-            assert.equal(game.roundInfo.team0.bid.blind, true);
-            done();
-        });
+    describe("bid method", function (done) {
+        var actions;
 
-        it("should return the correct actions when asked to show cards", function (done) {
-            var actions = game.bid("team0", spades.bidType["show-cards"]);
+        it("should return 3 actions and set blind to false when a team wants to show cards", function (done) {
+            actions = game.bid("team0", spades.bidType["show-cards"]);
             assert.equal(actions.length, 3);
             assert.equal(actions[0].action, spades.SERVER_ACTION.SEND_CARDS);
             assert.deepEqual(actions[0].recipient, {team: "team0", player: "player0"});
+            assert.equal(actions[1].action, spades.SERVER_ACTION.SEND_CARDS);
+            assert.deepEqual(actions[1].recipient, {team: "team0", player: "player1"});
             assert.equal(actions[2].action, spades.SERVER_ACTION.PROMPT_BID);
             assert.equal(actions[2].recipient, "team0");
             assert.equal(game.roundInfo.team0.bid.blind, false);
+            done();
+        });
+
+        it("should return 1 action and store bid information when a team bids after seeing their cards", function (done) {
+            actions = game.bid("team0", spades.bidType["show-cards"]);
             actions = game.bid("team0", spades.bidType["board"]);
             assert.equal(actions.length, 1);
             assert.equal(actions[0].action, spades.SERVER_ACTION.PROMPT_BID);
             assert.equal(actions[0].recipient, "team1");
             assert.equal(game.roundInfo.team0.bid.val, 4);
             assert.equal(game.roundInfo.team0.bid.mult, 1);
+            done();
+        });
+
+        it("should return 3 actions and store bid information if a team bids blind", function (done) {
+            actions = game.bid("team0", spades.bidType["show-cards"]);
+            actions = game.bid("team0", spades.bidType["board"]);
             actions = game.bid("team1", spades.bidType["2-for-10"]);
             assert.equal(actions.length, 3);
             assert.equal(actions[0].action, spades.SERVER_ACTION.SEND_CARDS)
@@ -72,7 +83,7 @@ describe("Model's", function () {
         });
     });
 
-    describe("books", function (done) {
+    describe("determineWinner method", function (done) {
         var make_book = function (a, b, c, d) {
             return [
                 {card: new card.Card(a)},
@@ -86,12 +97,18 @@ describe("Model's", function () {
             assert.equal(game.determineWinner(book).card.id, gold_winner);
         };
 
-        it("should go to the winning player's team", function (done) {
+        it("should pick the highest card played", function (done) {
             check_winner(make_book(12, 11, 10, 9), 12);
+            done();
+        });
+        it("should make the suit of the first card trump other suites except spades", function (done) {
             check_winner(make_book(12, 13, 10, 9), 12);
+            check_winner(make_book(4, 13, 11, 10), 11);
+            done();
+        });
+        it("should make spades trump all other suits", function (done) {
             check_winner(make_book(12, 50, 11, 10), 50);
             check_winner(make_book(50, 49, 11, 10), 50);
-            check_winner(make_book(4, 13, 11, 10), 11);
             done();
         });
     });
